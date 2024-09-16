089b9f32f3aed9c1ce4a1fde95aca39dde0f1d70
Madan-HM <Madan-HM>
2024-09-16 19:50:08 +0530

    created

--- initial codes of java/Firstcode.java
+++ initial codes of java/Firstcode.java
@@ -2,6 +2,6 @@ class Firstcode
 {
     public static void main(String args[])
     {
-        System.out.println("welcome to java");
+        System.out.println("welcome to java language");
     }
 }
