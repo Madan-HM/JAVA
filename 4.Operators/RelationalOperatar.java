36b375f2c9cb14d9cfea7546461027961b66745c
Madan-HM <Madan-HM>
2024-09-19 18:43:52 +0530

    added

---
+++ 4.Operators/RelationalOperatar.java
@@ -0,0 +1,9 @@
+class RelationalOperatar{
+    public static void main(String args[])
+    {
+        int x = 5;
+        int y = 4;
+        boolean result = x != y;
+        System.out.println(result);
+    }
+}
