2e610638b7a266ba7403a2a363bf043b28b08a7f
Madan-HM <Madan-HM>
2024-09-19 12:41:52 +0530

    added

---
+++ data types and varibales/Datatypes.java
@@ -0,0 +1,19 @@
+
+
+public class Datatypes {
+    public static void main(String args[])
+    {
+        int a=1;
+        byte b=127;
+        short c=558;
+        long d=5385l;
+
+        float e=5.f;
+        double f=5.8;
+
+        char g='g';
+        boolean h=true;
+
+    }
+    
+}
