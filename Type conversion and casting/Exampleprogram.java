6c68fcd51c80cb867f22f183255f9bb6bf1ac106
Madan-HM <Madan-HM>
2024-09-19 14:41:24 +0530

    added

---
+++ Type conversion and casting/Exampleprogram.java
@@ -0,0 +1,17 @@
+class Exampleprogram{
+    public static void main (String args[])
+    {
+        byte b = 127;
+        int a = b;
+        System.out.println(a);
+
+        int c = 257;
+        byte k = (byte)c;
+        System.out.println(k);
+
+        float f = 5.6f;
+        int i = (int)f;
+        System.out.println(i);
+         
+    }
+}
