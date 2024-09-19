5623b4190b8b3d1bd1b1b17ca6edc4f9d97c1eb7
Madan-HM <Madan-HM>
2024-09-19 13:35:58 +0530

    added

---
+++ data types and varibales/Literals.java
@@ -0,0 +1,27 @@
+
+
+public class Literals
+ {
+    public static void main(String args[])
+    {
+       int a=0B101;
+       int b=0X7E;
+       int c=10_00_00_000;
+       
+       double d=56;
+       double y=12e10;
+       
+       char g='a';
+       char h='a';
+       h++;
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(y);
+       System.out.println(g);
+       System.out.println(h);
+
+    }
+    
+}
