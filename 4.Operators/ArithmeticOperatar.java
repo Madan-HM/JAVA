7645ddb8cc04a04627090ead8914f459d05bdbae
Madan-HM <Madan-HM>
2024-09-19 18:26:09 +0530

    added

---
+++ 4.Operators/ArithmeticOperatar.java
@@ -0,0 +1,33 @@
+public class ArithmeticOperatar {
+    public static void main(String args[])
+    {
+        int a=7;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+
+        int d = a-b; 
+        System.out.println(d);
+
+        a = a+2;
+        System.out.println(a);
+
+        a += 2;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+
+        ++a;
+        System.out.println(a);
+
+        int m = ++a;
+        System.out.println(m);
+
+        int n = a++;
+        System.out.println(n);
+
+
+    }
+    
+}
