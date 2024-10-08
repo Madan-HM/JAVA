141478770306cc6c6a01c8d6787a3ad46e308dea
Madan H M <madan3.h.m@gmail.com>
2024-10-08 20:21:53 +0530

    added

---
+++ 5.Conditional statements/ifelse.java
@@ -0,0 +1,20 @@
+public class ifelse {
+    public static void main(String args[])
+    {
+        int x = 8;
+        int y = 7;
+
+        if(x>y)
+        {
+            System.out.println(x);
+
+        }
+        else
+        {
+            System.out.println(y);
+
+        }
+    }
+
+    
+}
