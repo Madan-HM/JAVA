f5d035c1a5418c968cad98121d10b4ccd60be1b4
Madan H M <madan3.h.m@gmail.com>
2024-10-08 20:42:44 +0530

    added

---
+++ TernaryOperator/operator.java
@@ -0,0 +1,14 @@
+
+
+public class operator {
+    public static void main(String args[])
+    {
+        int n=4;
+        int result = 0;
+
+        result =( n%2 == 0)? 10:20;
+        System.out.println(result);
+
+    }
+    
+}
