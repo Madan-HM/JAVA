787e83c64dd66bce781b441ede764bc79146e63c
Madan H M <madan3.h.m@gmail.com>
2024-10-08 21:20:43 +0530

    added

---
+++ 7.Switch statements/code.java
@@ -0,0 +1,36 @@
+public class code 
+{
+    public static void main(String a[])
+    {
+        int n = 7;
+        switch(n)
+        {
+            case 1:
+                   System.out.println("Monday");
+                   break;
+            case 2:
+                   System.out.println("Tuesday");
+                   break;
+            case 3:
+                   System.out.println("Wednesday");
+                   break;
+            case 4:
+                   System.out.println("thursday");
+                   break;
+            case 5:
+                   System.out.println("Friday");
+                   break;
+            case 6:
+                   System.out.println("Saturday");
+                   break;
+            case 7:
+                   System.out.println("Sunday");
+                   break;
+            default:
+                   System.out.println("Enter valid number:");
+                   break;
+                  
+        }
+    }
+    
+}
