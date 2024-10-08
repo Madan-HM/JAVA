2f3245f8c16cd18a27f59283e5825529ec80e0e0
Madan H M <madan3.h.m@gmail.com>
2024-10-08 20:22:07 +0530

    added

---
+++ 5.Conditional statements/ifelseif.java
@@ -0,0 +1,18 @@
+public class ifelseif {
+    public static void main(String args[])
+    {
+        int x = 8;
+        int y = 7;
+        int z = 9;
+
+        if(x>y && x>z)
+         System.out.println(x);
+        else if(y>z)
+         System.out.println(y);
+        else  
+         System.out.println(z);
+          
+          
+    }
+    
+}
