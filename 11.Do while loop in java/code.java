e1a64455d8c3f520c8b7d607718e05d6b923c7d2
Madan H M <madan3.h.m@gmail.com>
2024-10-10 14:45:59 +0530

    added

---
+++ 11.Do while loop in java/code.java
@@ -0,0 +1,14 @@
+public class code {
+    public static void main(String[] args)
+
+    {
+        int i=7;
+
+        do
+        {
+            System.out.println("hi " +i);
+            i++;
+        }while(i<=5);
+    }
+    
+}
