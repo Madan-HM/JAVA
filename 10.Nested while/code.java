4b22fc05980f4e74dbb085a5af76183f6277c41c
Madan H M <madan3.h.m@gmail.com>
2024-10-10 14:45:52 +0530

    added

---
+++ 10.Nested while/code.java
@@ -0,0 +1,19 @@
+public class code {
+    public static void main(String[] args)
+    {
+        int i=1;
+        while (i <= 5)
+        {
+            System.out.println("hi " +i);
+            int j=1;
+            while(j <= 3)
+            {
+                System.out.println("hello " + j);
+                j++;
+
+            }
+            i++;
+        }
+    }
+    
+}
