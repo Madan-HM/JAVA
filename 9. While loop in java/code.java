a729ffff2d15112b099a88dba95fa6e2b71c3f7b
Madan H M <madan3.h.m@gmail.com>
2024-10-10 14:46:40 +0530

    added

---
+++ 9. While loop in java/code.java
@@ -0,0 +1,16 @@
+public class code 
+{
+    public static void main(String[] args)
+    {
+          int i=1;
+
+          while(i <= 4)
+          {
+            System.out.println("hi " + i);
+            i++;
+          };
+          System.out.println("bye " +i);
+    }
+    
+    
+}
