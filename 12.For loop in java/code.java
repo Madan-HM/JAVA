10cbb1345cd15acb425fdb277dfa0e9ec0ae5388
Madan H M <madan3.h.m@gmail.com>
2024-10-10 14:46:09 +0530

    added

---
+++ 12.For loop in java/code.java
@@ -0,0 +1,12 @@
+public class code
+{
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println("Welcome");
+       
+        }
+    }
+    
+}
