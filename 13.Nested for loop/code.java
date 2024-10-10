4f653b442814bb654716970cf5b21ef60bc440ee
Madan H M <madan3.h.m@gmail.com>
2024-10-10 14:46:16 +0530

    added

---
+++ 13.Nested for loop/code.java
@@ -0,0 +1,14 @@
+public class code 
+{
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=5; i++)
+        {
+         System.out.println("Day" +i);
+         for(int j=9; j<=18; j++)
+         {
+            System.out.println(j+" - "+(j+1));
+         }
+       }
+    }
+}
