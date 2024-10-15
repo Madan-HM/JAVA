43c3b481cc1e7fcb42de1c402090e3072c725ec7
Madan H M <madan3.h.m@gmail.com>
2024-10-15 15:41:05 +0530

    edit

---
+++ Array of objects in java/code.java
@@ -0,0 +1,18 @@
+class student
+{
+    int rollno;
+    String name;
+    int marks;
+}
+public class code 
+{
+    public static void main(String[] args)
+    {
+        student obj=new student();
+        obj.rollno =1;
+        obj.name = maa;
+        obj.marks = 90;
+
+    }
+    
+}
