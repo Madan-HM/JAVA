6b68234be5ad18173e72a1eeef25bfbc9bcad94b
Madan H M <madan3.h.m@gmail.com>
2024-10-15 12:08:39 +0530

    added

---
+++ 18.Array in java/forblock.java
@@ -0,0 +1,20 @@
+public class forblock
+{
+    public static void main(String[] args)
+    {
+        int num[] = new int[4];
+
+        num[0]=14;
+        num[1]=23;
+        num[2]=37;
+        num[3]=45;
+
+        for(int i=0;i<4;i++)
+        {
+
+        System.out.println(num[i]);
+
+        }
+    }
+    
+}
