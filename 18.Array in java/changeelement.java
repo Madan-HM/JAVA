b70e09b34c3dad7836321f2c41e5b6dfc1094f0a
Madan H M <madan3.h.m@gmail.com>
2024-10-15 12:08:25 +0530

    added

---
+++ 18.Array in java/changeelement.java
@@ -0,0 +1,11 @@
+public class changeelement
+{
+    public static void main(String[] args)
+    {
+        int num[] = {1,2,3};
+        num[1] = 6;
+
+        System.out.println(num[1]);
+    }
+    
+}
