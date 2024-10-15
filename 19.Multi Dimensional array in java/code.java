eb7ed65815a6601e89562fc185394221781ec8b4
Madan H M <madan3.h.m@gmail.com>
2024-10-15 13:17:41 +0530

    added

---
+++ 19.Multi Dimensional array in java/code.java
@@ -0,0 +1,18 @@
+public class code 
+{
+    public static void main(String[] args)
+    {
+        int num [][] = new int[2][4];
+
+        for( int i=0 ; i<2; i++)
+        {
+            for(int j=0; j<4;j++)
+            {
+                System.out.print(num[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
