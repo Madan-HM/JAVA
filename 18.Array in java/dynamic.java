14471f66da427ee3b234cb4def575c830132a560
Madan H M <madan3.h.m@gmail.com>
2024-10-15 12:08:34 +0530

    added

---
+++ 18.Array in java/dynamic.java
@@ -0,0 +1,19 @@
+public class dynamic 
+{
+    public static void main(String[] args)
+    {
+        int num[] = new int[4];
+
+        num[0]=12;
+        num[1]=25;
+        num[2]=33;
+        num[3]=44;
+
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+
+    }
+    
+}
