3d03ed286bc66f41c48a643ab3ff851a80327b60
Madan H M <madan3.h.m@gmail.com>
2024-10-16 10:02:56 +0530

    added

---
+++ 23.String in java/has.java
@@ -0,0 +1,16 @@
+public class has
+{
+    
+        public static void main(String[] args)
+        {
+            String name=new String("abc"); //In stack memory reference variable name have some address
+                                                    //new string here object is created in heap memory with some address 
+
+            System.out.println(name);
+            System.out.println(name.hashCode());
+                                        // c is capital
+        }
+   
+    
+}
+       
