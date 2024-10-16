63551de2030c4889a6820190bd4a238ce53dacec
Madan H M <madan3.h.m@gmail.com>
2024-10-16 09:33:51 +0530

    added

---
+++ 22.Enchanced  for loop in java/code.java
@@ -0,0 +1,23 @@
+
+public class code 
+{
+    public static void main(String[] args)
+    {
+        int num[]=new int[3];
+        num[0]=23;
+        num[1]=33;
+        num[2]=43;
+
+       // for(int i=0;i<3;i++)
+       //  {
+       //     System.out.println(num[i]);
+       //  }
+        
+       for(int n:num)
+       {
+        System.out.println(n);
+       }
+
+    }
+    
+}
