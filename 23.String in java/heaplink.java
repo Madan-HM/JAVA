ee08d8813c5bf33dc26c207681d8d2bb4ab6b0ca
Madan H M <madan3.h.m@gmail.com>
2024-10-16 10:03:02 +0530

    added

---
+++ 23.String in java/heaplink.java
@@ -0,0 +1,14 @@
+public class heaplink 
+{
+    
+        public static void main(String[] args)
+        {
+            String name=new String("abc"); //In stack memory reference variable name have some address
+                                                    //new string here object is created in heap memory with some address 
+
+            System.out.println(name);
+        }
+   
+    
+}
+                                      
