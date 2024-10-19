9559a1529094c18f06d30dc66e6d67c5b5f0d640
Madan H M <madan3.h.m@gmail.com>
2024-10-19 12:20:07 +0530

    added

--- 36.Multi-Level  inheritance in java/code.java
+++ 36.Multi-Level  inheritance in java/code.java
@@ -2,6 +2,17 @@ public class code
 {
     public static void main(String[] args)
     {
+        Veryadvcalc obj=new Veryadvcalc();
+
+        int r1=obj.aDD(1, 2);
+        int r2=obj.sUB(4, 2);
+        int r3=obj.mUL(2, 2);
+        int r4=obj.dIV(8, 2);
+        int r5=obj.minimum(7, 2);
+
+        System.out.println(r1 + " " +r2+" "+r3+" "+r4+" "+r5);
+
+
         
     }
 }
