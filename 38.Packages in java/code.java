a10af7fabbc56fe32da0827380e1c38da8772a0c
Madan H M <madan3.h.m@gmail.com>
2024-10-19 15:14:48 +0530

    added

---
+++ 38.Packages in java/code.java
@@ -0,0 +1,27 @@
+//import tools.Calc;     // here . refers to next file  
+//import tools.AdvCalc;
+
+import tools.*;           // to import all the files in the tools package put *
+public class code 
+{
+    public static void main(String[] args)
+    
+    {
+       AdvCalc obj=new AdvCalc();        //here we just create object for subclass
+
+       int r1 = obj.aDD(2, 03);
+       System.out.println("addtion ="+ r1);
+
+       int r2 = obj.sUB(20, 10);
+       System.out.println("substraction ="+ r2);
+
+       
+
+       int r3 = obj.dIV(15, 3);
+       System.out.println("division ="+ r3);
+
+       int r4 = obj.mUL(5, 2);
+       System.out.println("multiplicaton ="+ r4);
+    }
+    
+}
