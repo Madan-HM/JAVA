2302d1b8797b0394046d2f5792cba0dde98b35b7
Madan H M <madan3.h.m@gmail.com>
2024-10-19 17:08:49 +0530

    added

---
+++ 40.Final Keyword in java/Finalvariable.java
@@ -0,0 +1,14 @@
+
+
+public class Finalvariable
+{
+    public static void main(String[] args)
+    {
+        final int a = 9;  //if we finalize the variable we can't able to change the value of it
+        a=10;             //here error shows at a
+
+        System.out.println(a);
+    }
+
+    
+}
