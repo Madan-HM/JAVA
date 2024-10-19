bdfe38e6debebe682b3df7a2deb279e0fda364b3
Madan H M <madan3.h.m@gmail.com>
2024-10-19 15:14:51 +0530

    added

---
+++ 38.Packages in java/tools/AdvCalc.java
@@ -0,0 +1,20 @@
+package tools;  //declaring the package named tools, here below class come under this package  
+                //once declared drag and drop the files into tools package
+public class AdvCalc extends Calc 
+{
+  
+    public int dIV(int a,  int b)
+
+    {
+        return a/b;
+
+    }
+    public int mUL(int a,  int b)
+
+    {
+        return a*b;
+
+    }
+ 
+    
+}
