bc00af1e13000086185fbd47e5f4c3e8e4c04d75
Madan H M <madan3.h.m@gmail.com>
2024-10-19 11:52:49 +0530

    added

---
+++ 44.Inheritence in java/AdvCalc.java
@@ -0,0 +1,18 @@
+public class AdvCalc extends Calc //AdvCalc is subclass and Calc is superclass
+{
+  
+    public int dIV(int a,  int b)
+
+    {
+        return a/b;
+
+    }
+    public int mUL(int a,  int b)
+
+    {
+        return a*b;
+
+    }
+ 
+    
+}
