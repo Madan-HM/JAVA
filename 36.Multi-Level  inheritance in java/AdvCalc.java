1763a096c1fd7a770737e51c8d2cf9f3281796b2
Madan H M <madan3.h.m@gmail.com>
2024-10-19 12:19:58 +0530

    added

---
+++ 36.Multi-Level  inheritance in java/AdvCalc.java
@@ -0,0 +1,12 @@
+public class AdvCalc extends Calc
+{
+    public int mUL(int a, int b)
+    {
+        return a*b;
+    }
+    public int dIV(int a, int b)
+    {
+        return a/b;
+    }
+    
+}
