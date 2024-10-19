bb1431af42b8a905f0495d213e7da118e9e11302
Madan H M <madan3.h.m@gmail.com>
2024-10-19 15:14:55 +0530

    added

---
+++ 38.Packages in java/tools/Calc.java
@@ -0,0 +1,18 @@
+package tools;     
+
+public class Calc 
+{
+    public int aDD(int a,  int b)
+
+    {
+        return a+b;
+
+    }
+    public int sUB(int a,  int b)
+
+    {
+        return a-b;
+
+    }
+    
+}
