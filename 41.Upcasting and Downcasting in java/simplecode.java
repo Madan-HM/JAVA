86e04d44c1f1cfea2035913491955ce7c1d571ac
Madan H M <madan3.h.m@gmail.com>
2024-10-20 12:38:39 +0530

    added

---
+++ 41.Upcasting and Downcasting in java/simplecode.java
@@ -0,0 +1,14 @@
+public class simplecode
+{
+    public static void main(String[] args)
+    {
+    double d =12;
+    int i=(int)d;  //Upcasting
+    
+    double a= (double)i;//downcasting
+
+    System.out.println(a);
+    System.out.println(i);
+    }
+    
+}
