3774d9552909517848d19c50b9efcd7d61f4765f
Madan H M <madan3.h.m@gmail.com>
2024-10-23 18:33:20 +0530

    added

---
+++ 66.Race Condition in java/code.java
@@ -0,0 +1,43 @@
+class counter
+{
+    int count;
+    public synchronized void increment()
+    {
+        count++;
+    }
+}
+
+public class code 
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        counter c=new counter();
+
+        Runnable obj1=()->
+        {
+            for(int i=1;i<=1000;i++)
+            {
+                c.increment();
+            }
+        };
+        Runnable obj2 = ()->
+        {
+            for(int i=1;i<=1000;i++)
+            {
+                c.increment();
+            }
+        };
+        
+        Thread t1=new Thread(obj1); 
+        Thread t2=new Thread(obj2);
+      
+        t1.start();  
+        t2.start(); 
+        t1.join();
+        t2.join();
+        System.out.println(c.count);
+    
+
+    }
+    
+}
